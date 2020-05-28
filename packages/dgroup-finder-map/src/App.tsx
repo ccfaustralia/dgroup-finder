269538fbfb0c8d9366b5289e9a78552b16d19275
Abigail Fuentes <abigailberylfuentes@gmail.com>
2020-05-28 21:33:50 +1000

    [map] rendering map WIP

--- packages/dgroup-finder-map/src/App.tsx
+++ packages/dgroup-finder-map/src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './App.module.css';
 import Search from "./components/search/Search";
 import ResultsList from "./components/resultsList/ResultsList";
 import ResultsMap from "./components/resultsMap/ResultsMap";
 
+export interface IAppState {
+    googleServices?: any;
+}
 function App() {
+
+    const [appState, setAppState] = useState<IAppState>({});
+
+    const apiHasLoaded = (map: any, maps: any) => {
+        setAppState({
+            ...appState,
+            googleServices: {
+            maps,
+            autoCompleteService: new maps.places.AutocompleteService(),
+            placesService: new maps.places.PlacesService(map),
+            directionService: new maps.DirectionsService(),
+            geoCoderService: new maps.Geocoder(),
+            singaporeLatLng: new maps.LatLng(1.3521, 103.8198)
+        }});
+    };
+
   return (
     <div className={styles.container}>
       <div id="search-container" className={styles.search}><Search /></div>
       <div id="results-list-container" className={styles.list}><ResultsList /></div>
-      <div id="results-map-container" className={styles.map}><ResultsMap /></div>
+      <div id="results-map-container" className={styles.map}><ResultsMap onApiLoad={apiHasLoaded}/></div>
     </div>
   );
 }
--- packages/dgroup-finder-map/src/components/resultsMap/ResultsMap.tsx
+++ packages/dgroup-finder-map/src/components/resultsMap/ResultsMap.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
+import styles from './ResultsMap.module.css';
 
-function ResultsMap() {
-    return (<p>ResultsMap</p>)
+export const ResultsMap: React.FunctionComponent<any> = ({
+    onApiLoad
+}) => {
+    const key = 'AIzaSyDzcwstCPNO69m6a84kcXmoaVvb-5wTYqw';
+    return(
+        <section className={styles.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: key,
+                    libraries: ['places', 'directions']
+                }}
+                defaultZoom={4} // Supports DP, e.g 11.5
+                defaultCenter={{ lat: -24.85733, lng: 133.85750 }}
+                yesIWantToUseGoogleMapApiInternals={true}
+                onGoogleApiLoaded={({ map, maps }) => onApiLoad(map, maps)}
+            >
+            </GoogleMapReact>
+        </section>
+    );
 }
 
 export default ResultsMap;

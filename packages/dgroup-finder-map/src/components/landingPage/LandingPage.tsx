05f0378819015c832420e3732c2d1114f5309704
Elliott Miller <elliottbmiller@gmail.com>
2020-08-03 19:49:48 +1000

    [MAP] Added CCF logo to landing page.

--- packages/dgroup-finder-map/src/components/landingPage/LandingPage.tsx
+++ packages/dgroup-finder-map/src/components/landingPage/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from './LandingPage.module.css';
 import Search from "../search/Search";
+import ccfLogo from '../../assets/ccf_logo_white_sinlge_sm.png'
 
 export const LandingPage: React.FunctionComponent<any> = ({appState, setAppState}) => {
     return (
         <div className={`${styles.landingPage} ${appState.homeMarker ? styles.hidden : styles.visible}`} >
+            <img src={ccfLogo} alt={'Christ\'s Commission Fellowship'} />
             <h1>Find a dgroup</h1>
             <p>Enter your location to find a Dgroup near you...</p>
             <Search appState={appState} setAppState={setAppState}/>

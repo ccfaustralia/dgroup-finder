d1c86bf953cdddf7620bcfd949b86938687c587f
Elliott Miller <elliottbmiller@gmail.com>
2020-08-11 19:49:02 +1000

    [MAP] Add initial Dgroup Contact Form.

--- packages/dgroup-finder-map/src/components/contactForm/ContactForm.tsx
+++ packages/dgroup-finder-map/src/components/contactForm/ContactForm.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Modal from "../modal/Modal";
+import styles from './ContactForm.module.css'
+
 
 export const ContactForm: React.FunctionComponent<any> = ({
-
+    dgroup,
+    onCloseModal
 }) => {
 
     const handleSubmit = () => {
@@ -9,29 +13,33 @@ export const ContactForm: React.FunctionComponent<any> = ({
     };
 
     return(
-        <form onSubmit={handleSubmit}>
-            <div className="inputGroup">
-                <label htmlFor="name">Name</label>
-                <input type="text"/>
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="email">Email</label>
-                <input type="text"/>
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="mobile">Mobile</label>
-                <input type="text"/>
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="gender">Gender</label>
-                <input type="text"/>
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="message">Message</label>
-                <textarea/>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <Modal title='Join DGroup' onClose={onCloseModal}>
+            <form className={styles.contactForm} onSubmit={handleSubmit}>
+                <div className="input-group grid-full-width">
+                    <label htmlFor="name">Name</label>
+                    <input type="text"/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="text"/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="mobile">Mobile</label>
+                    <input type="text"/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="gender">Gender</label>
+                    <input type="text"/>
+                </div>
+                <div className="input-group grid-full-width">
+                    <label htmlFor="message">Message</label>
+                    <textarea/>
+                </div>
+                <div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </Modal>
     )
 };
 
--- packages/dgroup-finder-map/src/components/infoWindow/InfoWindow.tsx
+++ packages/dgroup-finder-map/src/components/infoWindow/InfoWindow.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './InfoWindow.module.css'
+import ContactForm from "../contactForm/ContactForm";
 
 
 export const InfoWindow: React.FunctionComponent<any> = ({dgroup}) => {
+    const [showContactModal, setShowContactModal] = useState<boolean>(false);
+
     return (
         <div className={styles.infoWindowContainer}>
             <h2>
@@ -10,6 +13,8 @@ export const InfoWindow: React.FunctionComponent<any> = ({dgroup}) => {
             </h2>
             <i>{dgroup.lifeStage}</i>
             <p>{dgroup.description}</p>
+            <button onClick={() => setShowContactModal(true)}>Join This DGroup</button>
+            {showContactModal && <ContactForm dgroup={dgroup} onCloseModal={() => setShowContactModal(false)}/>}
         </div>
     );
 };
